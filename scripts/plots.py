cece7f63ad560a6fd35cd1c5ac42ca40d86ce9fc
Gunjan Dhanuka <lucifergd192@gmail.com>
2022-06-27 18:59:55 +0530

    refactored into folders

---
+++ scripts/plots.py
@@ -0,0 +1,213 @@
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+import pickle
+import streamlit as st
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from gensim.models import Word2Vec
+import pandas as pd
+
+def display_scatterplot_2D(model, user_input=None, words=None, label=None, color_map=None, annotation='On', dim_red = 'PCA', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
+    
+    if words == None or len(words)==0:
+        if sample > 0:
+            words = np.random.choice(list(list(model.wv.index_to_key)), 40)
+        else:
+            words = [ word for word in model.vocab ]
+    
+    word_vectors = np.array([model.wv[w] for w in words])
+    
+    if dim_red == 'PCA':
+        two_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:2]
+    else:
+        two_dim = TSNE(random_state=0, perplexity = perplexity, learning_rate = learning_rate, n_iter = iteration).fit_transform(word_vectors)[:,:2]
+
+    
+    data = []
+    count = 0
+    for i in range (len(user_input)):
+
+                trace = go.Scatter(
+                    x = two_dim[count:count+topn,0], 
+                    y = two_dim[count:count+topn,1],  
+                    text = words[count:count+topn] if annotation == 'On' else '',
+                    name = user_input[i],
+                    textposition = "top center",
+                    textfont_size = 15,
+                    mode = 'markers+text',
+                    marker = {
+                        'size': 15,
+                        'opacity': 0.8,
+                        'color': 2
+                    }
+       
+                )
+               
+                data.append(trace)
+                count = count+topn
+
+    trace_input = go.Scatter(
+                    x = two_dim[count:,0], 
+                    y = two_dim[count:,1],  
+                    text = words[count:],
+                    name = 'input words',
+                    textposition = "top center",
+                    textfont_size = 15,
+                    mode = 'markers+text',
+                    marker = {
+                        'size': 25,
+                        'opacity': 1,
+                        'color': 'black'
+                    }
+                    )
+
+    data.append(trace_input)
+    
+# Configure the layout.
+    layout = go.Layout(
+        margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
+        showlegend=True,
+        hoverlabel=dict(
+            bgcolor="white", 
+            font_size=20, 
+            font_family="Times New Roman"),
+        legend=dict(
+        x=1,
+        y=0.5,
+        font=dict(
+            family="Times New Roman",
+            size=25,
+            color="black"
+        )),
+        font = dict(
+            family = " Times New Roman ",
+            size = 15),
+        autosize = False,
+        width = 1000,
+        height = 1000
+        )
+
+
+    plot_figure = go.Figure(data = data, layout = layout)
+    plot_figure.update_layout(template='plotly_dark')
+    st.plotly_chart(plot_figure)
+
+
+def display_scatterplot_3D(model, user_input=None, words=None, label=None, color_map=None, annotation='On',  dim_red = 'PCA', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
+    
+    if words == None or len(words)==0:
+        if sample > 0:
+            words = np.random.choice(list(list(model.wv.index_to_key)), 100)
+        else:
+            words = [ word for word in model.vocab ]
+    
+    word_vectors = np.array([model.wv[w] for w in words])
+    
+    if dim_red == 'PCA':
+        three_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:3]
+    else:
+        three_dim = TSNE(n_components = 3, random_state=0, perplexity = perplexity, learning_rate = learning_rate, n_iter = iteration).fit_transform(word_vectors)[:,:3]
+
+    color = '#f63366'
+    quiver = go.Cone(
+        x = [0,0,0], 
+        y = [0,0,0],
+        z = [0,0,0],
+        u = [0.1,0,0],
+        v = [0,0.1,0],
+        w = [0,0,0.1],
+        anchor = "tail",
+        colorscale = [[0, color] , [1, color]],
+        showscale = False
+        )
+    
+    data = [quiver]
+
+    count = 0
+    for i in range (len(user_input)):
+
+                trace = go.Scatter3d(
+                    x = three_dim[count:count+topn,0], 
+                    y = three_dim[count:count+topn,1],  
+                    z = three_dim[count:count+topn,2],
+                    text = words[count:count+topn] if annotation == 'On' else '',
+                    name = user_input[i],
+                    textposition = "top center",
+                    textfont_size = 20,
+                    mode = 'markers+text',
+                    marker = {
+                        'size': 10,
+                        'opacity': 0.8,
+                        'color': 2
+                    }
+       
+                )
+               
+                data.append(trace)
+                count = count+topn
+
+    trace_input = go.Scatter3d(
+                    x = three_dim[count:,0], 
+                    y = three_dim[count:,1],  
+                    z = three_dim[count:,2],
+                    text = words[count:],
+                    name = 'input words',
+                    textposition = "top center",
+                    textfont_size = 20,
+                    mode = 'markers+text',
+                    marker = {
+                        'size': 10,
+                        'opacity': 1,
+                        'color': 'black'
+                    }
+                    )
+
+    data.append(trace_input)
+    
+# Configure the layout.
+    layout = go.Layout(
+        margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
+        showlegend=True,
+        legend=dict(
+        x=1,
+        y=0.5,
+        font=dict(
+            family="Times New Roman",
+            size=25,
+            color="black"
+        )),
+        font = dict(
+            family = " Times New Roman ",
+            size = 15),
+        autosize = False,
+        width = 1000,
+        height = 1000
+        )
+
+
+    plot_figure = go.Figure(data = data, layout = layout)
+    plot_figure.update_layout(template='plotly_dark')
+    st.plotly_chart(plot_figure)
+
+def horizontal_bar(word, similarity):
+    
+    similarity = [ round(elem, 3) for elem in similarity ]
+    
+    data = go.Bar(
+            x= similarity,
+            y= word,
+            orientation='h',
+            text = similarity,
+            marker_color= '#f63366',
+            textposition='auto')
+
+    layout = go.Layout(
+            font = dict(size=20),
+            xaxis = dict(showticklabels=False, automargin=True),
+            yaxis = dict(showticklabels=True, automargin=True,autorange="reversed"),
+            margin = dict(t=20, b= 20, r=10)
+            )
+
+    plot_figure = go.Figure(data = data, layout = layout)
+    st.plotly_chart(plot_figure)
